a2490dc7f1465c2da5e7cdf2cbf9ecd18762cdda
mikhailkolesnikov <mikhail.kolesnikov@outlook.de>
2024-06-27 18:09:56 +0200

    add if

--- src/MessageApp.java
+++ src/MessageApp.java
@@ -3,21 +3,13 @@ import java.util.Scanner;
 public class MessageApp {
 
     public static void main(String[] args) {
-        ;
 
-        System.out.println("Для ввода сообщения нажмите 1, для ввода цыфр введите 2 :");
+
+        System.out.println("Для ввода сообщения нажмите 1, для ввода цифр введите 2 :");
         Scanner scanner = new Scanner(System.in);
         int counter = scanner.nextInt();
         scanner.nextLine();
-        //       Message<String> str = new Message<>(scanner.nextLine());
-//        String string = str.getMessage();
-        //System.out.println(string);
 
-//        Message<Integer> integerMessage = new Message<>(scanner.nextInt());
-//        Integer integer = integerMessage.getMessage();
-        //System.out.println(integer);
-//
-//
         if (counter == 1) {
 
             Message<String> str = new Message<>(scanner.nextLine());
@@ -41,3 +33,13 @@ public class MessageApp {
 
     }
 }
+
+//       Message<String> str = new Message<>(scanner.nextLine());
+//        String string = str.getMessage();
+//System.out.println(string);
+
+//        Message<Integer> integerMessage = new Message<>(scanner.nextInt());
+//        Integer integer = integerMessage.getMessage();
+//System.out.println(integer);
+//
+//
