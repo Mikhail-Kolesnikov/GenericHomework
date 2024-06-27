ef5c0dd5e5fd54eb43660785fdf5bc487f33f26c
mikhailkolesnikov <mikhail.kolesnikov@outlook.de>
2024-06-27 17:48:09 +0200

    add if

--- src/MessageApp.java
+++ src/MessageApp.java
@@ -3,24 +3,39 @@ import java.util.Scanner;
 public class MessageApp {
 
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        Message <String> str =new Message<>(scanner.nextLine());
-        String string= str.getMessage();
-        System.out.println(string);
+        ;
 
+        System.out.println("Для ввода сообщения нажмите 1, для ввода цыфр введите 2 :");
+        Scanner scanner = new Scanner(System.in);
+        int counter = scanner.nextInt();
+        //       Message<String> str = new Message<>(scanner.nextLine());
+//        String string = str.getMessage();
+        //System.out.println(string);
 
-        Message<Integer> integerMessage= new Message<>(scanner.nextInt());
-        Integer integer= integerMessage.getMessage();
-        System.out.println(integer);
+//        Message<Integer> integerMessage = new Message<>(scanner.nextInt());
+//        Integer integer = integerMessage.getMessage();
+        //System.out.println(integer);
+//
+//
+        if (counter == 1) {
 
+            Message<String> str = new Message<>(scanner.nextLine());
 
+            String string = str.getMessage();
 
+            System.out.println(string);
 
+        }
+        if (counter == 2) {
+            Message<Integer> integerMessage = new Message<>(scanner.nextInt());
+            Integer integer = integerMessage.getMessage();
 
+            System.out.println(integer);
 
-
+        } else {
+            System.out.println("Введите 1 или 2");
+        }
 
 
     }
 }
-
